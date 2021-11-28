25403728893d892d8b0b0627925aacc6de2f74f9
MoMeak9 <1308994506@qq.com>
2021-11-29 00:48:44 +0800

    feat:更新考试卷内容

--- pages/questionList.js
+++ pages/questionList.js
@@ -1,18 +1,6 @@
 export default [
   {
     type: 'input',
-    title: '正版ID',
-    subtitle: '正版ID',
-    content: '',
-  },
-  {
-    type: 'input',
-    title: '正版绑定邮箱',
-    subtitle: '正版ID',
-    content: '',
-  },
-  {
-    type: 'input',
     title: '真实年龄',
     subtitle: '周岁哦！',
     content: '',
@@ -20,16 +8,9 @@ export default [
   {
     type: 'input',
     title: '入正时间',
-    subtitle: '周岁哦！',
+    subtitle: '年月',
     content: '',
-  },
-  {
-    type: 'input',
-    title: '擅长干啥？',
-    subtitle: '红石？建筑？冒险？',
-    content: '',
-  },
-  {
+  }, {
     type: 'select',
     isMultiple: false,
     title: '是否加入过其他正版公益类服务器？',
@@ -47,8 +28,7 @@ export default [
         label: '否',
       }
     ]
-  },
-  {
+  }, {
     type: 'select',
     isMultiple: false,
     title: '1+2+3+4+5+6.......+71=？',
@@ -59,24 +39,23 @@ export default [
     subItem: [
       {
         value: 'A',
-        label: '216556',
+        label: '2488',
       },
       {
         value: 'B',
-        label: '916454',
+        label: '2485',
       }, {
         value: 'C',
-        label: '5648',
+        label: '2359',
       }, {
         value: 'D',
         label: '老子不会',
       }
     ]
-  },
-  {
+  }, {
     type: 'select',
     isMultiple: false,
-    title: 'i的87次方等于？',
+    title: 'i的2次方等于？',
     subtitle: '高中数学，虚数计算',
     content: '',
     solution: 'B',
@@ -84,18 +63,159 @@ export default [
     subItem: [
       {
         value: 'A',
-        label: '216556',
+        label: 'i',
       },
       {
         value: 'B',
-        label: '916454',
+        label: '-1',
       }, {
         value: 'C',
-        label: '5648',
+        label: '1',
       }, {
         value: 'D',
         label: '老子不会',
       }
     ]
-  }
+  }, {
+    type: 'select',
+    isMultiple: false,
+    title: '怪物能生成在下台阶（半砖）上？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'B',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '对',
+      },
+      {
+        value: 'B',
+        label: '错',
+      }
+    ]
+  }, {
+    type: 'select',
+    isMultiple: false,
+    title: '钻石稿子在白板情况下挖石头最快？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'A',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '对',
+      }, {
+        value: 'B',
+        label: '错',
+      }
+    ]
+  }, {
+    type: 'select',
+    isMultiple: false,
+    title: '鞘翅会生成在什么建筑物中？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'C',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '末地门',
+      }, {
+        value: 'B',
+        label: '地牢',
+      }, {
+        value: 'C',
+        label: '末地船',
+      }, {
+        value: 'D',
+        label: '腐竹家',
+      }
+    ]
+  }, {
+    type: 'select',
+    isMultiple: false,
+    title: '以下那个是幻翼膜的用途？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'A',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '炼药',
+      }, {
+        value: 'B',
+        label: '制作鞘翅',
+      }, {
+        value: 'C',
+        label: '吃',
+      }, {
+        value: 'D',
+        label: '合成装备',
+      }
+    ]
+  },{
+    type: 'select',
+    isMultiple: false,
+    title: '北极熊可以繁殖？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'B',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '对',
+      }, {
+        value: 'B',
+        label: '错',
+      }
+    ]
+  },{
+    type: 'select',
+    isMultiple: false,
+    title: '蓝冰由什么合成？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'A',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '浮冰',
+      }, {
+        value: 'B',
+        label: '冰块',
+      }, {
+        value: 'C',
+        label: '雪',
+      }
+    ]
+  },{
+    type: 'select',
+    isMultiple: false,
+    title: '以下那个是幻翼膜的用途？',
+    subtitle: '请认证回答',
+    content: '',
+    solution: 'A',
+    score: 10,
+    subItem: [
+      {
+        value: 'A',
+        label: '炼药',
+      }, {
+        value: 'B',
+        label: '制作鞘翅',
+      }, {
+        value: 'C',
+        label: '吃',
+      }, {
+        value: 'D',
+        label: '合成装备',
+      }
+    ]
+  },
 ]
