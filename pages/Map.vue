ea417bb92fdf1befc5b184c3f9911c22dc3856b7
MoMeak9 <1308994506@qq.com>
2021-12-04 11:47:58 +0800

    feat:服务器简介界面

---
+++ pages/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="width: 100%;height: 100%">
+    <iframe src="http://166.111.50.77:33334/" width="100%" height="100%"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map"
+}
+</script>
+
+<style scoped>
+
+</style>
