fcec75580b30b04e32ea7e31541edd8c1fa34844
MoMeak9 <1308994506@qq.com>
2022-02-10 00:55:46 +0800

    feat:持续更新...

--- plugins/route.ts
+++ plugins/route.ts
@@ -1,8 +1,10 @@
 import storage from 'store'
+import {NavigationGuardNext, Route} from 'vue-router'
 
-export default ({app}) => {
+export default ({app}: any) => {
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  app.router.beforeEach((to, from, next) => {
+  app.router.beforeEach((to: Route, from: Route, next: NavigationGuardNext) => {
     const token = storage.get('token')
     if (to.path === '/questionnaire' && token == null) {
       next({
--- store/getters.ts
+++ store/getters.ts
@@ -1,5 +1,5 @@
 export default {
-  token: state => state.token,
-  userInfo: state => state.userInfo,
-  userName: state => state.userInfo.userName
+  token: (state:any) => state.token,
+  userInfo: (state:any) => state.userInfo,
+  userName: (state:any) => state.userInfo.userName
 }
