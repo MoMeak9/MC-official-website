07eb0b2391981a229647b10c36dc4809b0799044
jiaze <1244153559@qq.com>
2022-06-16 02:22:52 +0800

    用户个人界面的嵌套路由的框架

---
+++ pages/user/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-center">
+    <h1>轻微的轻微的</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {
+  },
+  data: () => {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.user-center {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ pages/user/message.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-center">
+    <h1>给腐竹留言</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {
+  },
+  data: () => {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.user-center {
+  text-align: center;
+  padding: 20px;
+}
+</style>
